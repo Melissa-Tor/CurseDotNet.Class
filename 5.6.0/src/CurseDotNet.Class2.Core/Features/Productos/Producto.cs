e3c1922f388f6480ddc932a555354c7e3e5eea01
Melissa Peralta <eperalta.melissa@outlook.com>
2020-10-09 21:54:24 -0600

    Tercer commit

---
+++ 5.6.0/src/CurseDotNet.Class2.Core/Features/Productos/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities;
+
+namespace CurseDotNet.Class2.Features.Productos
+{
+    public class Producto : Entity
+    {
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Existencia { get; set; }
+        public bool Activo { get; set; }
+    }
+}
+ 
